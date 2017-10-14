5abf5b4426117e16d16bfed5cc01096e56894f09
rightfold <rightfold@gmail.com>
2017-10-14 13:30:12 +0200

    [runtime] Remove runtime in favor of VM

--- runtime/src/context.cpp
+++
@@ -1,8 +0,0 @@
-#include <niobium/context.hpp>
-
-#include <chrono>
-#include <string>
-
-nb::context::call_info::call_info(std::chrono::steady_clock::time_point timestamp)
-  : timestamp(timestamp) {
-}
